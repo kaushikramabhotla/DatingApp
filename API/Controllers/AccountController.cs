16819c552a079ed79e9b5f9c112996c345e2c4f9
Kaushik Ramabhotla <ramabhotlakaushik@gmail.com>
2024-05-26 14:19:55 +0530

    End of Section 4

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,78 @@
+﻿using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseController
+    {
+        private readonly DataContext _dataContext;
+
+        private readonly ITokenService _tokenService;
+
+        public AccountController(DataContext dataContext, ITokenService tokenService)
+        {
+            _dataContext = dataContext;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("register")] //api/account/register
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            if(await UserExists(registerDto.Username)) return BadRequest("Username is already taken !");
+            using var hmac = new HMACSHA512();
+
+            var newUser = new AppUser
+            {
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+            _dataContext.Users.Add(newUser);
+            await _dataContext.SaveChangesAsync();
+
+            // A new JWT token is created by using the 'TokenService' service by passing the new user
+            // A new UserDto is returned to client which has the value of the username and the JWT token
+            return new UserDto 
+            {
+                Username = newUser.UserName,
+                Token = _tokenService.CreateToken(newUser)
+            };
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _dataContext.Users.SingleOrDefaultAsync(x=> x.UserName == loginDto.Username);
+
+            if (user == null) return Unauthorized("Invalid Username !");
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for(int i = 0;i< computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password !");
+            }
+
+            // A new JWT token is created by using the 'TokenService' service by passing the user
+            // A new UserDto is returned to client which has the value of the username and the JWT token
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
+        }
+
+        private async Task<bool> UserExists(string _username)
+        {
+            return await _dataContext.Users.AnyAsync(x => x.UserName == _username.ToLower());
+        }
+    }
+}
---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,38 @@
+﻿using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config) 
+        {
+            //This is for DB CRUD
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddScoped<ITokenService, TokenService>();
+
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                    };
+                });
+            return services;
+        }
+    }
+}
